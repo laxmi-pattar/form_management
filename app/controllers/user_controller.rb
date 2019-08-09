ac25edc17b1b0e37dbdf0e3666a23164c6d1c09f
Laxmi Pattar <laxmipattar710@gmail.com>
2019-08-09 14:12:37 +0530

    adding rubocop

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,25 @@
+class UserController < ApplicationController
+  before_action :set_user
+  
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @user.update(user_params)
+
+    redirect_to user_show_path
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :city, :address)
+  end
+end
